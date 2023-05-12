573b0dc1a2af0c628f6e4347826ba09c009cdeb8
yeomjaeseung <127087166+yeomjaeseung@users.noreply.github.com>
2023-05-12 23:32:19 +0900

    Update payment.sol

--- payment.sol
+++ payment.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 import "./ownerRegistration.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/math/SafeMath.sol";
 
 contract Payment is OwnerRegistration {
 
@@ -77,6 +77,25 @@ contract Payment is OwnerRegistration {
         uint256 refundAmount = reservation.depositAmount;
         reservation.depositAmount = 0;
         reservation.user.transfer(refundAmount);
+    }
+    // 예약 상태 변경 함수
+    function updateReservationStatus(uint256 reservationId, ReservationStatus newStatus) external {
+        Reservation storage reservation = reservations[reservationId];
+
+        // 변경하려는 사용자가 예약한 사용자 혹은 가게 주인인지 확인
+        require(
+            msg.sender == reservation.user || msg.sender == reservation.owner,
+            "Only the user or the owner can update the reservation status."
+        );
+
+        // 새로운 상태가 유효한 상태인지 확인
+        require(
+            uint(newStatus) >= 0 && uint(newStatus) <= 4,
+            "Invalid reservation status."
+        );
+
+        // 상태 업데이트
+        reservation.status = newStatus;
     }
 
 }
