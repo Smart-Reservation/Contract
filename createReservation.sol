dfe52536116f337b5e545f3343058a5c176cf22b
yeomjaeseung <127087166+yeomjaeseung@users.noreply.github.com>
2023-05-14 15:23:39 +0900

    Update createReservation.sol

--- createReservation.sol
+++ createReservation.sol
@@ -18,45 +18,23 @@ contract CreateReservation {
     // 예약 ID를 키로 하여 예약 정보를 저장하는 매핑
     mapping(uint256 => Reservation) public reservations;
 
-    // 다음 예약 ID
-    uint256 public nextReservationId;
-
     // 예약 생성 이벤트
     event ReservationCreated(uint256 id, address user);
 
     // 예약 생성 함수
-    function createReservation(address payable _owner, uint256 _depositAmount) external {
+    function createReservation(uint256 _reservationId, address payable _owner, uint256 _depositAmount) external {
+        // 기존에 동일한 ID로 예약이 존재하는지 확인
+        require(reservations[_reservationId].user == address(0), "Reservation with this ID already exists.");
+
         // 새로운 예약 생성
-        Reservation storage newReservation = reservations[nextReservationId];
-        newReservation.id = nextReservationId;
+        Reservation storage newReservation = reservations[_reservationId];
+        newReservation.id = _reservationId;
         newReservation.depositAmount = _depositAmount;
         newReservation.user = payable(msg.sender);
         newReservation.owner = _owner;
         newReservation.status = ReservationStatus.Pending;
 
         // 예약 생성 이벤트 발생
-        emit ReservationCreated(nextReservationId, msg.sender);
-
-        // 다음 예약 ID 갱신
-        nextReservationId++;
-    }
-        // 예약 상태 변경 함수
-    function updateReservationStatus(uint256 reservationId, ReservationStatus newStatus) external {
-        Reservation storage reservation = reservations[reservationId];
-
-        // 변경하려는 사용자가 예약한 사용자 혹은 가게 주인인지 확인
-        require(
-            msg.sender == reservation.user || msg.sender == reservation.owner,
-            "Only the user or the owner can update the reservation status."
-        );
-
-        // 새로운 상태가 유효한 상태인지 확인
-        require(
-            uint(newStatus) >= 0 && uint(newStatus) <= 4,
-            "Invalid reservation status."
-        );
-
-        // 상태 업데이트
-        reservation.status = newStatus;
+        emit ReservationCreated(_reservationId, msg.sender);
     }
 }
